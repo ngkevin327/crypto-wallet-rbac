0b8e057144543248012f66d4043ae1f1923588f3
Kevin Ng <ngkevin3327@gmail.com>
2024-03-04 11:20:00 -0500

    chore: add shared typescript, eslint, and prettier configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "**/dist/**",
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/coverage/**",
+    ],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+    },
+  }
+);
---
+++ packages/policy-engine/src/index.ts
@@ -0,0 +1,1 @@
+export const WTP_POLICY_ENGINE_VERSION = "0.1.0";
